d3b00d7b966231f4e7493ccfec4e0249b4dc998a
syed salman <syedsalman3753@gmail.com>
2023-10-13 18:23:09 +0530

    [MOSIP-29245] Added chart testing tool

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -185,13 +185,13 @@ upload() {
   tmpDir=$(mktemp -d)
   pushd $tmpDir >& /dev/null
 
+  echo "========== xxxxx ============================"
   git clone ${REPO_URL}
   cd ${REPOSITORY}
   git config user.name "${COMMIT_USERNAME}"
   git config user.email "${COMMIT_EMAIL}"
   git remote set-url origin ${REPO_URL}
   git checkout ${BRANCH}
-  git config --global --add safe.directory /github/workspace
 
   charts=$(cd ${CHARTS_TMP_DIR} && ls *.tgz | xargs)
 
